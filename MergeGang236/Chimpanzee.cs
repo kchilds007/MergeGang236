ce75034bd0e18c10bca1df71c0a1eec384891a20
Kentaro Childs <kchilds@chapman.edu>
2026-04-21 15:06:08 -0700

    Deleted chimpanzee

--- MergeGang236/Chimpanzee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GitPractice;
-
-public class Chimpanzee : Animal
-{
-    public override void MakeSound()
-    {
-        Console.WriteLine("The chimpanzee says: I just ripped your face off because maybe don't keep primates as pets");
-    }
-}
